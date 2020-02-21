8512bea94e68f14119e69dae1ab88f01457ce8a0
Vincent Prouillet <balthek@gmail.com>
2020-02-21 13:42:00 +0100

    Remove failure attributes

--- src/lib.rs
+++ src/lib.rs
@@ -12,28 +12,23 @@ pub type Result<T> = StdResult<T, TaxonomyError>;
 #[derive(Debug)]
 pub enum TaxonomyError {
     /// The given key could not be found in the taxonomy.
-    // #[fail(display = "Key is not valid: {}", key)]
     NoSuchKey {
         key: String,
     },
     /// A string could not be parsed into a TaxRank.
-    // #[fail(display = "Rank is not supported: {}", rank)]
     UnrecognizedRank {
         rank: String,
     },
     /// A string could not be parsed into a TaxRank.
-    // #[fail(display = "Error in file {} at line {}: {}", file, line, msg)]
     ImportError {
         line: usize,
         msg: String,
     },
     /// The taxonomy has an unrooted node or some other issue that breaks
     /// the assumption its a tree
-    // #[fail(display = "Taxonomy is not a tree; broken at {}", tax_id)]
     MalformedTree {
         tax_id: String,
     },
-    // #[fail(display = "Each taxa must have only one {}", field)]
     CreationFailed {
         field: String,
     },
